737f391e68da7e63c910b69f7bd065dcc58ad50b
Larry Bruce Daniel <larry@refusetoown.com>
2026-06-12 13:27:05 -0400

    Added static to arrays to allow for lengthening.

--- isomorphic.c
+++ isomorphic.c
@@ -10,16 +10,16 @@
  * @return True if the strings are isomorphic, false otherwise.
  */
 bool is_isomorphic(const char* s1, const char* s2) {
-    int len1 = strlen(s1);
-    int len2 = strlen(s2);
+   int len1 = strlen(s1);
+   int len2 = strlen(s2);
 
 
     // Use arrays as hash maps to store character mappings
     // Assuming ASCII characters, we can use an array of size 128 (or 256)
-    int map1[128] = {0}; // Maps characters from s1 to s2
-    int map2[128] = {0}; // Maps characters from s2 to s1 (to ensure one-to-one mapping)
+    static int map1[128] = {0}; // Maps characters from s1 to s2
+    static int map2[128] = {0}; // Maps characters from s2 to s1 (to ensure one-to-one mapping)
     
-      static int leni;
+    static int leni;
 
     if (len1>len2) leni=len2;
     else leni=len1;
